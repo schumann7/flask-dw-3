d3973dc87ef357fe39f4e3e4bebe3cca768a1828
schumann7 <gabrielschumannsc@gmail.com>
2025-03-27 14:53:34 -0300

    aula 27/03/2025

--- metodoPost.py
+++ metodoPost.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from uuid import uuid4 as uuid
 
 app = Flask(__name__)
 
@@ -14,16 +15,39 @@ def somar():
     num1 = dados_recebidos['num1']
     num2 = dados_recebidos['num2']
     resultado = num1 + num2
-    historico.append({f"{num1} + {num2}": resultado})
+    id = str(uuid())
+    historico.append({
+        'id':id,
+        f"{num1} + {num2} = ":resultado 
+        })
     return {"resultado": resultado}
-
-@app.route("/somados", methods=["GET"])
-def somados():
-    dados_recebidos = request.get_json()
-    num1 = dados_recebidos['num1']
-    num2 = dados_recebidos['num2']
-    return {"Número 1 = ": num1, "Número 2 = ": num2}
 
 @app.route("/historico", methods=["GET"])
 def get_historico():
     return historico
+
+@app.route("/deletar/<id>", methods=["DELETE"])
+def deletar(id):
+    global historico
+    historico = [d for d in historico if d.get('id') != id]
+    return  "Deletado com sucesso"
+
+@app.route("/editar/<id>", methods=["PUT"])
+def editar(id):
+    global historico
+    dados = request.get_json()
+    num1 = dados["num1"]
+    num2 = dados["num2"]
+
+    lista_temp = []
+    for calculo in historico:
+        if calculo['id'] == id:
+            resultado = num1 + num2
+            lista_temp.append({
+                "id":id,
+                f"{num1} + {num2} = ":resultado 
+            })
+        else:
+            lista_temp.append(calculo)
+    historico = lista_temp
+    return {}
